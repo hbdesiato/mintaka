747711de41bcc6dd48bf043550c79db56765ce01
desiato <hbdesiato@gmx.de>
2025-10-09 23:58:39 +0200

    remove cosmic-desktop

--- build_files/build.sh
+++ build_files/build.sh
@@ -11,7 +11,7 @@ set -ouex pipefail
 
 # this installs a package from fedora repos
 # dnf5 install -y tmux 
-dnf5 install -y @cosmic-desktop-environment
+# dnf5 install -y @cosmic-desktop-environment
 
 # Use a COPR Example:
 #
@@ -24,4 +24,4 @@ dnf5 install -y @cosmic-desktop-environment
 
 # systemctl enable podman.socket
 systemctl disable gdm.service
-systemctl enable cosmic-greeter.service
+# systemctl enable cosmic-greeter.service
